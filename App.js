8212e134e83a6d2f49d2b450d9ada8b2e4ca152d
Drake <drakebennion@gmail.com>
2024-01-08 13:31:32 -0700

    set up navigation

--- App.js
+++ App.js
@@ -1,14 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false}}>
+        <Stack.Screen 
+          name="Home"
+          component={HomeScreen}
+        />
+        <Stack.Screen 
+          name="Profile"
+          component={SecondaryScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>  
+      <Text>Get ready for Gradeness</Text>
+      <Button 
+        title="Go to secondary"
+        onPress={() => navigation.navigate('Profile', { name: 'Drake' })}
+      />
     </View>
   );
 }
+
+const SecondaryScreen = ({ navigation, route }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Hi { route.params.name }!</Text>
+      <Button 
+        title="Go home"
+        onPress={() => navigation.navigate('Home')}
+      />
+    </View>
+  );
+}
+
 
 const styles = StyleSheet.create({
   container: {
