dbe48acfc746c9822288377fb841707bcb640e52
Drake <drake.bennion@gmail.com>
2024-01-29 18:22:46 -0700

    feat: edit task

--- src/Roadmap/TaskScreen.tsx
+++ src/Roadmap/TaskScreen.tsx
@@ -1,20 +1,36 @@
-import { Button } from "@react-native-material/core";
+import { Button, TextInput } from "@react-native-material/core";
 import { doc, getFirestore, updateDoc } from "firebase/firestore";
-import { ScrollView, StyleSheet, Text } from "react-native"
+import { useState } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native"
 
 export const TaskScreen = ({ navigation, route }) => {
     const db = getFirestore();
     const { taskId, objective, semester, complete } = route.params;
+    const [editMode, setEditMode] = useState(false);
+    const [task, setTask] = useState({
+        objective,
+    });
 
     const toggleComplete = async () => {
         const taskRef = doc(db, 'tasks', taskId);
         await updateDoc(taskRef, { complete: !complete });
     };
 
-    return (
-        <ScrollView contentContainerStyle={styles.container}>
-            <Text>{ objective }</Text>
+    const updateTaskWithDatabase = async () => {
+        // todo: is there a better way of not duplicating this?
+        const taskRef = doc(db, 'tasks', taskId);
+
+        await updateDoc(taskRef, task);
+    }
+
+    const ReadView = () => (
+        <View>
+            <Text>{ task.objective }</Text>
             <Text>{ semester }</Text>
+            <Button 
+                title="Edit"
+                onPress={() => { setEditMode(true); }}
+            />
             <Button 
                 title={ complete ? "Mark as uncomplete" : "Mark as complete" }
                 onPress={() => toggleComplete().then(navigation.pop)}
@@ -23,6 +39,30 @@ export const TaskScreen = ({ navigation, route }) => {
                 title="Go back"
                 onPress={() => navigation.pop()}
             />
+        </View>
+    );
+
+    const EditView = () => (
+        <View>
+            <TextInput 
+                label="Task"
+                value={task.objective}
+                onChangeText={(objective) => setTask({ ...task, objective })}
+            />
+            <Button 
+                title="Save"
+                onPress={() => updateTaskWithDatabase().then(() => setEditMode(false))}
+            />
+            <Button 
+                title="Cancel"
+                onPress={() => { setEditMode(false); }}
+            />
+        </View>
+    );
+
+    return (
+        <ScrollView contentContainerStyle={styles.container}>
+            { editMode ? EditView() : ReadView() }
         </ScrollView>
     )
 }
