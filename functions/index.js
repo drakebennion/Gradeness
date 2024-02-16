34e8f76859491760153e869aaa7d0a7a580cf1db
Drake <drake.bennion@gmail.com>
2024-02-15 22:00:54 -0700

    add accomplishment object on signup

--- functions/index.js
+++ functions/index.js
@@ -11,11 +11,31 @@ exports.createUserActivities = auth.user().onCreate(async (user) => {
       .collection("testActivities")
       .get();
 
+  await createAccomplishmentForUser(user);
+
   Promise.all(
       testActivities.docs.map(createTestActivityForUser(user)),
   )
       .catch(logger.error);
 });
+
+const createAccomplishmentForUser = async (user) => {
+  await getFirestore()
+      .collection("accomplishments")
+      .add({
+        userId: user.uid,
+        createdAt: Date.now(),
+        createdBy: -1,
+        updatedAt: Date.now(),
+        updatedBy: -1,
+        content: {
+          9: "",
+          10: "",
+          11: "",
+          12: "",
+        },
+      });
+};
 
 const createTestActivityForUser = (user) => async (testActivity) => {
   const {year, semester, name, id, order, overview, description} =
